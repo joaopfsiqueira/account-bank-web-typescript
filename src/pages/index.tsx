b9691f4962ecf54214864e0c8b3f09d99ce85e6d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-20 14:40:45 -0300

    feat: first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Index: NextPage = () => {
+  return <h1 className="text-red-500 text-xl">Hello world</h1>;
+};
+
+// const Home: NextPage = () => {
+//   return <h1 className="text-red-500 text-xl">Hello world</h1>;
+// };
+
+// export default Home;
+
+export default Index;
