996721abea0ccb5697216ddbedd22c5c89a77ffd
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-25 01:48:59 -0300

    feat: criando nova homepage

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import Dashboard from '../components/Dashboard';
+import Header from '../components/Header';
+import NewTransactionModal from '../components/NewTransactionModal';
+
+import { TransactionsProvider } from '../hooks/useTransactions';
+
+Modal.setAppElement('#__next');
+
+export default function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+    </TransactionsProvider>
+  );
+}
