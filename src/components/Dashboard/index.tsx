bbe5ee31825feb65c0d0e04154af59489a1104db
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-27 16:15:19 -0300

    feat: tratando tamanho de transactions

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Summary from '../Summary';
 import TransactionsTable from '../TransactionsTable';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+  const { transactions } = useTransactions();
   return (
     <Container>
       <Summary />
-      <TransactionsTable />
+      {transactions.length != 0 && (
+        <TransactionsTable transactions={transactions} />
+      )}
     </Container>
   );
-}
+};
 
 export default Dashboard;
