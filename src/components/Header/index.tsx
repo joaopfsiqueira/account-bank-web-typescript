15ffd042e24e354daab7e0dfa0b3037db7e5d89a
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-27 15:46:50 -0300

    feat: criando filtros

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import { Container, Content } from './styles';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 interface HeaderProps {
   onOpenNewTransactionModal(): void;
 }
 
 const Header: React.FC<HeaderProps> = ({ onOpenNewTransactionModal }) => {
+  const { signOut } = useContext(AuthContext);
+  const { filterByDate } = useTransactions();
+  const [date, setDate] = useState<string>('');
+
   return (
     <Container>
       <Content>
         <button type="button" onClick={onOpenNewTransactionModal}>
           Nova Transação
         </button>
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          onBlur={() => filterByDate(date)}
+        ></input>
+        <button onClick={signOut}>Deslogar</button>
       </Content>
     </Container>
   );
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -9,6 +9,12 @@ import { setCookie, parseCookies } from 'nookies'; //usando nookies para salvar 
 
 interface TransactionsContextData {
   transactions: Transaction[];
+  transactionsCashin: Transaction[];
+  transactionsCashout: Transaction[];
+  filterCashin(): Promise<void>;
+  filterCashout(): Promise<void>;
+  clearFilter(): Promise<void>;
+  filterByDate(date: string): Promise<void>;
   createTransaction(transaction: TransactionInput): Promise<void>;
 }
 
@@ -16,7 +22,7 @@ const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
-interface Transaction {
+export interface Transaction {
   value: number;
   debitedAccount: number;
   creditedAccount: number;
@@ -37,6 +43,12 @@ export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({
   children,
 }) => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactionsCashin, setTransactionsCashin] = useState<Transaction[]>(
+    []
+  );
+  const [transactionsCashout, setTransactionsCashout] = useState<Transaction[]>(
+    []
+  );
 
   useEffect(() => {
     const { 'account-bank-token': token } = parseCookies();
@@ -58,7 +70,7 @@ export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({
   async function createTransaction(transaction: TransactionInput) {
     const { 'account-bank-token': token } = parseCookies();
     (async function insertTransaction(): Promise<void> {
-      await fetch('http://localhost:4000/transactions/users', {
+      await fetch('http://localhost:4000/transactions', {
         headers: {
           'Content-Type': 'application/json',
           Authorization: 'Bearer ' + token,
@@ -72,8 +84,9 @@ export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({
       // The return value is *not* serialized
       // You can return Date, Map, Set, etc.
     })();
-    (async function returnTransactions(): Promise<void> {
-      const res = await fetch('http://localhost:4000/transactions/users', {
+
+    setTimeout(async function returnTransactions(): Promise<void> {
+      const res = await fetch('http://localhost:4000/transactions/user', {
         headers: {
           'Content-Type': 'application/json',
           Authorization: 'Bearer ' + token,
@@ -84,7 +97,108 @@ export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({
       // You can return Date, Map, Set, etc.
       const result = await res.json();
       setTransactions(result);
+    }, 500);
+  }
+
+  useEffect(() => {
+    (async function returnTransactionsCashOut(): Promise<void> {
+      const { 'account-bank-token': token } = parseCookies();
+      const res = await fetch(
+        'http://localhost:4000/transactions/user/cashout',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+          },
+          method: 'GET',
+        }
+      );
+      // The return value is *not* serialized
+      // You can return Date, Map, Set, etc.
+      const result = await res.json();
+      setTransactionsCashout(result);
     })();
+
+    (async function returnTransactionsCashin(): Promise<void> {
+      const { 'account-bank-token': token } = parseCookies();
+      const res = await fetch(
+        'http://localhost:4000/transactions/user/cashin',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+          },
+          method: 'GET',
+        }
+      );
+      // The return value is *not* serialized
+      // You can return Date, Map, Set, etc.
+      const result = await res.json();
+      setTransactionsCashin(result);
+    })();
+  }, [transactions]);
+
+  async function filterCashin(): Promise<void> {
+    const { 'account-bank-token': token } = parseCookies();
+    const res = await fetch('http://localhost:4000/transactions/user/cashin', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      method: 'GET',
+    });
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+    const result = await res.json();
+    setTransactions(result);
+  }
+
+  async function filterCashout(): Promise<void> {
+    const { 'account-bank-token': token } = parseCookies();
+    const res = await fetch('http://localhost:4000/transactions/user/cashout', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      method: 'GET',
+    });
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+    const result = await res.json();
+    setTransactions(result);
+  }
+
+  async function filterByDate(date: string): Promise<void> {
+    const { 'account-bank-token': token } = parseCookies();
+    const res = await fetch('http://localhost:4000/transactions/user/bydate', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      body: JSON.stringify({
+        date,
+      }),
+      method: 'GET',
+    });
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+    const result = await res.json();
+    setTransactions(result);
+  }
+
+  async function clearFilter(): Promise<void> {
+    const { 'account-bank-token': token } = parseCookies();
+    const res = await fetch('http://localhost:4000/transactions/user', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      method: 'GET',
+    });
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+    const result = await res.json();
+    setTransactions(result);
   }
 
   return (
@@ -92,6 +206,12 @@ export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({
       value={{
         transactions,
         createTransaction,
+        transactionsCashout,
+        transactionsCashin,
+        filterCashin,
+        filterCashout,
+        clearFilter,
+        filterByDate,
       }}
     >
       {children}
