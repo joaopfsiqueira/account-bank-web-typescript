285fa5a18562d3e0cbece73e1b9d1baa617ad86d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-27 16:15:03 -0300

    feat: fazendo logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 import { loginUser, userInformations } from '../pages/api/Apis';
-import { setCookie, parseCookies } from 'nookies'; //usando nookies para salvar os cookies. // lendo os cookies com parseCookies
+import { setCookie, parseCookies, destroyCookie } from 'nookies'; //usando nookies para salvar os cookies. // lendo os cookies com parseCookies
 import Router from 'next/router';
 
 type UserToken = {
@@ -10,7 +10,8 @@ type UserToken = {
 type AuthContextType = {
   user: UserToken | null;
   isAuthenticated: boolean;
-  signIn: (body: Body) => Promise<void>;
+  signIn: (body: Body) => Promise<void | any>;
+  signOut: () => Promise<void>;
 };
 
 type Body = {
@@ -36,21 +37,31 @@ export function AuthProvider({ children }: any) {
     }
   }, []);
 
-  async function signIn(body: Body) {
-    const { username, token } = await loginUser({
+  async function signIn(body: Body): Promise<void | any> {
+    const response = await loginUser({
       password: body.password,
       username: body.username,
     });
 
-    setCookie(undefined, 'account-bank-token', token, {
+    if (response.Message) {
+      return response;
+    }
+
+    setCookie(undefined, 'account-bank-token', response.token, {
       maxAge: 60 * 60 * 24, //24h
     });
 
-    setUser(username);
+    setUser(response.username);
+  }
+
+  async function signOut(): Promise<void> {
+    destroyCookie(undefined, 'account-bank-token');
+    setUser(null);
+    Router.push('/');
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
